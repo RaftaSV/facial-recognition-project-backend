fc039df9b9b1457e5f017e5f16dd241b0133eae2
Andres Cordova <andrescordovaoficial@gmail.com>
2023-06-05 20:22:42 -0600

    Adding Frontend Project. :package:

---
+++ frontend/src/components/delete.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Delete = () => {
+  return (
+    <div>Welcome to the new app!!</div>
+  )
+}
+
+export default Delete;
---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Delete from './components/delete';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Delete />
+  </React.StrictMode>
+);
