b3ecc2a571726a2049eb98c61ac5b744f47bd7e2
Mauri20 <pr0164032019@unab.edu.sv>
2023-06-07 22:31:18 -0600

    Some tests

--- frontend/src/components/delete.js
+++ frontend/src/components/delete.js
@@ -1,11 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Webcam from 'react-webcam';
 import * as faceapi from 'face-api.js';
 
 const Delete = () => {
-
   const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const canvas2Ref = useRef(null);
   const [photoBlob, setPhotoBlob] = useState(null);
+
+  useEffect(() => {
+    startApp();
+  }, []);
 
   const capturePhoto = async () => {
     const imageSrc = webcamRef.current.getScreenshot();
@@ -21,7 +26,7 @@ const Delete = () => {
 
   const drawPhoto = () => {
     if (photoBlob) {
-      const canvas = document.createElement('canvas');
+      const canvas = canvasRef.current;
       const ctx = canvas.getContext('2d');
       const img = new Image();
 
@@ -29,7 +34,6 @@ const Delete = () => {
         canvas.width = img.width;
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
-        document.body.appendChild(canvas);
       };
 
       const blobUrl = URL.createObjectURL(photoBlob);
@@ -42,8 +46,29 @@ const Delete = () => {
       faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
       faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
       faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-      faceapi.nets.faceExpressionNet.loadFromUri('/models')
-    ]);
+      faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    ]).then(() => {
+      faceDetector();
+    });
+  };
+  const faceDetector = async () => {
+    setInterval(async () => {
+      const detections = await faceapi
+        .detectAllFaces(
+          webcamRef.current,
+          new faceapi.TinyFaceDetectorOptions()
+        )
+        .withFaceLandmarks();
+      canvas2Ref.current.innerHTML = faceapi.createCanvasFromMedia(
+        webcamRef.current
+      );
+      faceapi.matchDimensions(canvasRef.current, webcamRef.current);
+      const resizedDetections = faceapi.resizeResults(detections, {
+        width: 320,
+        height: 240,
+      });
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+    }, 100);
   };
 
   const startApp = async () => {
@@ -51,19 +76,19 @@ const Delete = () => {
     console.log('Models loaded.');
   };
 
-  startApp();
-
   return (
     <div className="App">
       <h1>Face Recognition App</h1>
       <Webcam audio={false} ref={webcamRef} screenshotFormat="image/jpeg" />
+      <canvas id="canvas" ref={canvas2Ref}></canvas>
       <div>
         <button onClick={capturePhoto}>Tomar Foto</button>
         <button onClick={clearPhoto}>Limpiar</button>
         <button onClick={drawPhoto}>Dibujar Foto</button>
       </div>
+      <canvas id="canvas" ref={canvasRef}></canvas>
     </div>
   );
-}
+};
 
 export default Delete;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Enrollment from './pages/Enrollment';
+//import Enrollment from './pages/Enrollment';
+import Delete from './components/delete';
 import GlobalStyle from './styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <Enrollment />
+    {/* <Enrollment /> */}
+    <Delete />
   </React.StrictMode>
 );
